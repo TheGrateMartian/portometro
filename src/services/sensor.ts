ce000d03b442d072551e972b7f9d804e466c909b
TheGrateMartian <kidssmg@gmail.com>
2021-10-17 00:48:42 -0300

    *_*

--- src/services/sensor.ts
+++ src/services/sensor.ts
@@ -4,15 +4,16 @@ import * as util from 'util';
 
 const trig = 6;
 const echo = 13;
-await GPIO.setup(trig, GPIO.DIR_OUT);
-await GPIO.setup(echo, GPIO.DIR_IN);
-
-GPIO.output(trig, false);
+async function setup() {
+    await GPIO.setup(trig, GPIO.DIR_OUT);
+    await GPIO.setup(echo, GPIO.DIR_IN);
+    await GPIO.output(trig, false);
+}
 
 async function watcher() {
-    GPIO.output(trig, true);
+    await GPIO.output(trig, true);
     await delay(1);
-    GPIO.output(trig, false);
+    await GPIO.output(trig, false);
     let start = 0;
     let end = 0;
     while (await r() === false){
@@ -30,4 +31,4 @@ async function r(): Promise<boolean | undefined> {
     return await getInput(echo);
 }
 
-export {watcher};
+export {watcher, setup};

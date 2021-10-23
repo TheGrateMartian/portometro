5723dc4c657bea27097f9b64987a8f98356e2436
TheGrateMartian <kidssmg@gmail.com>
2021-10-22 23:46:43 -0300

    a

--- server/services/distanceSensor.js
+++ server/services/distanceSensor.js
@@ -6,24 +6,24 @@ const trigger = new Gpio(6, {mode: Gpio.OUTPUT});
 const echo = new Gpio(13, {mode: Gpio.INPUT, alert: true});
 trigger.digitalWrite(0);
 
-function watcher(callback) {
+function watcher()
+ {
     let startTick;
-    echo.once('alert', (level, tick) => {
+    echo.on('alert', (level, tick) => {
         if (level === 1) {
             startTick = tick;
         } else {
             console.log("i")
             const diff = (tick >> 0) - (startTick >> 0);
-            return callback(diff / 2 / millisecondsPerCm);
+            console.log(diff / 2 / millisecondsPerCm);
         }
     });
 }
-
+setInterval(() => {
+    trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+}, 1000);
 function activate() {
     trigger.trigger(10, 1);
-    watcher(function (err, data){
-        console.log(data);
-    })
 }
-activate()
+watcher()
 exports.activate = activate;
